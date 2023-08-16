c1e40c869474a3f6c3a2fb83a37d8c5e7ab46d77
Rifat Murtuza <33390000+rmrudro@users.noreply.github.com>
2023-08-16 17:05:01 +0900

    Add files via upload

---
+++ MergeTwoList.cs
@@ -0,0 +1,46 @@
+
+Merge Two Sorted Lists
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null)
+        {
+            if (l1.val < l2.val)
+            {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null)
+        {
+            current.next = l1;
+        }
+        else
+        {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
